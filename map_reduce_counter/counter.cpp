e88a0ab0aa5393db6f9cf1aa36f86ee130f146ed
Julio-Baeta1 <julbay1643@gmail.com>
2023-11-03 18:34:14 +0200

    Fix: Refactored code into mult sub-funcs

--- map_reduce_counter/counter.cpp
+++ map_reduce_counter/counter.cpp
@@ -5,43 +5,60 @@
 #include <map>
 #include <algorithm>
 #include <random>
+#include <memory>
+#include <functional>
 
 using namespace std;
+
+void printMap(map<int,int>* m){
+
+    int tol{0};
+
+    for_each(m->begin(), m->end(), 
+        [&tol](pair<int, int> p) { 
+            cout << p.first << ": " << p.second << endl; 
+            tol += p.second ;
+        }); 
+
+    printf("The total number of elements is: %d",tol);
+    
+}
+
+void genRandElems(vector<int> *x, int seed, int lim){
+    //Random generator
+    mt19937 r_gen (seed);
+    function<void(int&)> rGen = [&] (int& a) {a = r_gen()%lim;};
+
+    //Create vector of random positive ints less than lim=10 
+    for_each(x->begin(), x->end(), rGen);
+}
+
+void standardCount(map<int,int>* counter, int seed, int lim, int n_elem)
+{
+
+    vector<int> x(n_elem,0);
+    genRandElems(&x, seed,lim);
+
+    //If elem in counter freq++, else insert it into counter with freq=1
+    for(auto& elem: x)
+    {
+        auto iter = counter->find(elem);
+
+        if(iter != counter->end())
+            iter->second++;
+        else
+            counter->operator[](elem) = 1;        
+    }
+
+}
 
 
 int main(int argc, const char *argv[]){
 
     int seed{0}, lim{10}, n_elem{200};
-
-    //Random generator
-    mt19937 r_gen (seed);
-    auto rGen = [&] (int& a) {a = r_gen()%lim;};
-
-    //Create vector of random positive ints less than lim=10 
-    vector<int> x(n_elem,0);
-    for_each(x.begin(), x.end(), rGen);
-
-    map<int,int> counter;
-    map<int,int>::iterator iter;
-
-    //If elem in counter freq++, else insert it into counter with freq=1
-    for(auto& elem:x)
-    {
-        iter = counter.find(elem);
-
-        if(iter != counter.end())
-            iter->second++;
-        else
-            counter[elem] = 1;            
-    }
-
-    for_each(counter.begin(), counter.end(), 
-        [](pair<int, int> p) { 
-            cout << p.first << ": " << p.second << endl; 
-        }); 
-
-    
-
+    map<int,int> m;
+    standardCount(&m,seed,lim,n_elem);
+    printMap(&m);
     
     /*string test{"one two three one two one one two four three one two"};
     stringstream ss(test);
