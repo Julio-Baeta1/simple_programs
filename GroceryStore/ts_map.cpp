cbc22a3a75a2ae7693cb83c8370123a897176dbd
Julio-Baeta1 <julbay1643@gmail.com>
2023-11-22 20:53:42 +0200

    Add: atomic count to Grocery + customer thrd

--- GroceryStore/ts_map.cpp
+++ GroceryStore/ts_map.cpp
@@ -2,8 +2,10 @@
 #include <unordered_map>
 #include <string>
 #include <thread>
+#include <atomic>
 #include <mutex>
 #include <shared_mutex>
+#include <atomic>
 #include <vector>
 #include <algorithm>
 #include <chrono> 
@@ -14,6 +16,7 @@ using std::pair;
 using std::printf;
 using std::thread;
 using std::ref;
+using std::atomic;
 
 class TsMap
 {
@@ -24,11 +27,12 @@ class TsMap
     public:
         TsMap(){};
 
-        void populate(vector<string>* prod, vector<int>* amount)
+        void populate(vector<string>* prod, vector<int>* amount, atomic<int>& count)
         {
             std::unique_lock lock(shared_mut);
             for(int i=0; i < prod->size(); i++){
                 order_map[prod->at(i)] = amount->at(i);
+                count++;
             }
         }
 
@@ -61,21 +65,46 @@ class TsMap
             order_map[prod] = amount;        
         }
 
+        void removeEntry(string item)
+        {
+            std::unique_lock lock(shared_mut);
+            order_map.erase(item);
+        }
+
+        int size()
+        {
+            std::shared_lock lock(shared_mut);
+            return order_map.size();
+        }
+
 };
 
 
-static void getListItems(TsMap& orders, vector<string> items, int thrd_id)
+static void getListItems(TsMap& order, vector<string>& items, int thrd_id, atomic<int>& count)
 {
     pair<string,int> temp;
-    for(auto& item: items)
-    {
-        temp = orders.get(item);
+    while(count > 0){
+        for(auto& item: items)
+        {
+            temp = order.get(item);
 
-        if(temp.first != "Not found"){
-            printf("Thread %i going to get %iX %s\n",thrd_id,temp.second,temp.first.c_str());
-            std::this_thread::sleep_for (std::chrono::seconds(1));
-            printf("Thread %i got %iX %s\n",thrd_id,temp.second,temp.first.c_str());
+            if(temp.first != "Not found"){
+                count--;
+                std::this_thread::sleep_for (std::chrono::seconds(thrd_id*2));
+                printf("Thread %i got %iX %s\n",thrd_id,temp.second,temp.first.c_str());
+                order.removeEntry(temp.first);
+            }
         }
+    }
+}
+
+static void customer(TsMap& order,vector<string>& items,vector<int>& amounts,atomic<int>& count)
+{
+    for(int i=0; i < items.size(); i++){
+        std::this_thread::sleep_for (std::chrono::seconds(2));
+        order.addEntry(items.at(i),amounts.at(i));
+        count++;
+        printf("Customer added %iX %s to the order\n",amounts.at(i),items.at(i).c_str() );
     }
 }
 
@@ -83,20 +112,32 @@ int main(int argc, const char *argv[]){
     
     vector<string> order_product{"apple","bean can","beef","potatoes","chicken","floor cleaner","box pasta","washing powder"};
     vector<int> order_amount{2,5,2,6,1,3,2,1};
+    atomic<int> count{0};
 
     printf("Populates orders\n");
-    TsMap orders;
-    orders.populate(&order_product,&order_amount);
+    TsMap order;
+    order.populate(&order_product,&order_amount,count);
+
+    vector<string> extra_product{"1kg flour","tomatoes","fish","tomato sauce","watermelon"};
+    vector<int> extra_amount{1,2,1,3,1};
 
     vector<string> fruit_and_veg{"apple","pear","potatoes","tomatoes","watermelon","pumpkin"};
     vector<string> meats{"beef","chicken","pork","lamb","fish"};
+    vector<string> dry_goods{"box pasta","bean can","1kg flour","bag rice","tomato sauce"};
+    vector<string> cleaning{"washing powder","floor cleaner"};
 
     vector<thread> threads;
-    threads.push_back(thread(getListItems,ref(orders),fruit_and_veg,1));
-    threads.push_back(thread(getListItems,ref(orders),meats,2));
+    threads.push_back(thread(customer,ref(order),ref(extra_product),ref(extra_amount),ref(count)));
+
+    threads.push_back(thread(getListItems,ref(order),ref(fruit_and_veg),1,ref(count)));
+    threads.push_back(thread(getListItems,ref(order),ref(meats),2,ref(count)));
+    threads.push_back(thread(getListItems,ref(order),ref(dry_goods),3,ref(count)));
+    threads.push_back(thread(getListItems,ref(order),ref(cleaning),4,ref(count)));
     
     for(thread& thread_to_join : threads)
         thread_to_join.join();
+
+    std::printf("Thread safe Map size = %i\n", order.size());
 
     return 0;
 }
